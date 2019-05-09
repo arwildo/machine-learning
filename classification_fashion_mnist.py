f96acac70aaa42dedab2ac022e88dbcd7e923589
Arwildo <arwildo@gmail.com>
2019-05-09 16:00:33 +0800

    Creating a model

--- classification_fashion_mnist.py
+++ classification_fashion_mnist.py
@@ -13,8 +13,21 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 train_images = train_images/255.0
 test_images = test_images/255.0
 
-print(train_images[7])
 
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
 
-plt.imshow(train_images[7])
-plt.show()
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Tested Acc :', test_acc)
