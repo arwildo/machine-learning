7cb5b84bebf450eaad372838257fbcd6794a3408
arwildo <arwildo@gmail.com>
2019-11-27 23:00:15 +0800

    add two ml projects

---
+++ character-guesser/drawerApp.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+from tkinter import *
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pygame
+
+
+class pixel(object):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = (255, 255, 255)
+        self.neighbors = []
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, (self.x, self.y,
+            self.x + self.width, self.y + self.height))
+
+    def getNeighbors(self, g):
+        j = self.x // 20
+        i = self.y // 20
+        rows = 28
+        cols = 28
+
---
+++ movies-review/movies_review_classification.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+import tensorflow as td
+from tensorflow import keras
+import numpy as np
+
+data = keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
+
+print(train_data[0])
